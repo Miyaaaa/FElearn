3f899999cecc2da56803e2ed8b7f332013d6d4fa
Miyaaaa <583871106@qq.com>
2016-07-28 23:55:08 +0800

    complete form

--- task7/js/style_js.js
+++ task7/js/style_js.js
@@ -6,3 +6,22 @@ $(function(){
     var div4_form_width = $("#div4_form").width();
     $("#div4_form").css("left",(div4_pic1_width-div4_form_width)/2+"px");
 });
+function selclick(objstr){
+    switch(objstr){
+        case "country":
+            forselect(objstr,"for_country");break;
+        case "province":
+            forselect(objstr,"for_province");break;
+        case "city":
+            forselect(objstr,"for_city");break;
+    }
+}
+function forselect(selstr, ulstr){
+    //设置对应ul左上角位置
+    var selleft = document.getElementById(selstr).offsetLeft;
+    var seltop = document.getElementById(selstr).offsetTop;
+    $("#"+selstr).css("border" ,"1px solid #e74f4d");
+    $("#"+selstr).css("borderBottom", "none");
+    $("#"+ulstr).removeClass("hide");
+    $("#"+ulstr).css("left","275px");
+}
